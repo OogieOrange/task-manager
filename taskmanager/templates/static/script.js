a9278ed2116e4cf187166799c76ef5aad05c68d4
OogieOrange <>
2023-04-21 15:08:36 +0000

    test

---
+++ taskmanager/templates/static/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //sidenav initializaion
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
